ad6f863e44cbeb3bfd0b2bb78973bd52781f7644
stephen@complii.com.au <stephen@complii.com.au>
2015-07-06 00:08:06 +0800

    Created the project

---
+++ WebApi2OwinMiddleware/WebApi2OwinMiddleware/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Http;
+
+namespace WebApi2OwinMiddleware.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public string Get()
+        {
+            return DateTime.Now.ToLongTimeString();
+        }
+    }
+}
+
+
